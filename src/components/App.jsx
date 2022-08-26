572de62d6185e2bde1c006df59e128670c9220b7
Antonenko <42521426+167899@users.noreply.github.com>
2022-08-27 01:58:51 +0300

    fix app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,91 +1,70 @@
-import React, { Component } from 'react';
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+    ]
+  );
 
-    if (parsedContacts) {
-      this.setState({contacts: parsedContacts})
-    }
-  }
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  formSubmitState = data => {
-    if (!this.state.contacts.find(e => e.name.toLowerCase() === data.name.toLowerCase())) {
-      this.resetFilter();
-      this.setState(prevState => {
-        return {
-          contacts: [...prevState.contacts, data],
-        };
-      });
+  const formSubmitState = data => {
+    if (!contacts.find(e => e.name.toLowerCase() === data.name.toLowerCase())) {
+      resetFilter();
+      setContacts(prevState => [...prevState, data]);
     } else {
       alert('Нє... Уже є такий!');
     }
   };
 
-  filterState = filterValue => {
-    this.setState(() => {
-      return {
-        filter: filterValue,
-      };
-    });
+  const filterState = filterValue => {
+    setFilter(filterValue);
   };
 
-  resetFilter = () => {
-    this.setState({ filter: '' });
+  const resetFilter = () => {
+    setFilter('');
   };
 
-  delContact = e => {
-    this.setState(prevState => {
-      const delContacts = prevState.contacts.filter(
+  const delContact = e => {
+    setContacts(prevState => {
+      const delContact = prevState.filter(
         contact => contact.id !== e.target.parentElement.id && contact.id
       );
-
-      return {
-        contacts: delContacts,
-      };
+      return delContact;
     });
   };
 
-  render() {
-    const newContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const newContacts = () =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
 
-    return (
-      <div
-        style={{
-          height: '100vh',
-          marginLeft: '40px',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <Form onSubmit={this.formSubmitState} />
+  return (
+    <div
+      style={{
+        height: '100vh',
+        marginLeft: '40px',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <Form onSubmit={formSubmitState} />
 
-        <h2>Contacts</h2>
-        <Filter filterProp={this.filterState} value={this.state.filter} />
-        <ContactsList contacts={newContacts} del={this.delContact} />
-      </div>
-    );
-  }
-}
+      <h2>Contacts</h2>
+      <Filter filterProp={filterState} value={filter} />
+      <ContactsList contacts={newContacts()} del={delContact} />
+    </div>
+  );
+};
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Filter extends Component {
-  static propTypes = {
-    filterProp: PropTypes.func.isRequired,
-    value: PropTypes.string.isRequired,
+export const Filter = ({ filterProp, value }) => {
+
+  const filter = e => {
+    filterProp(e.currentTarget.value);
   };
 
-  hendleChange = e => {
-    this.filter(e);
-  };
+  return (
+    <label>
+      <h6
+        style={{
+          margin: '10px 0',
+          color: '#010101',
+        }}
+      >
+        Finde contact by name
+      </h6>
+      <input
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Search may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        value={value}
+        onChange={filter}
+      />
+    </label>
+  );
+};
 
-  filter = e => {
-    const filterValue = e.currentTarget.value;
-    this.props.filterProp(filterValue);
-  };
-
-  render() {
-    return (
-      <label>
-        <h6
-          style={{
-            margin: '10px 0',
-            color: '#010101',
-          }}
-        >
-          Finde contact by name
-        </h6>
-        <input
-          type="text"
-          name="filter"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Search may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          value={this.props.value}
-          onChange={this.hendleChange}
-        />
-      </label>
-    );
-  }
-}
+Filter.propTypes = {
+  filterProp: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
